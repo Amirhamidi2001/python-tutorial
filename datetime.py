9df587a6cf8cfb1cf76ee5130503f9804c292d7d
Amirhamidi2001 <amirarsalanh2001@gmail.com>
2024-05-13 18:52:20 +0330

    Python Datetime

---
+++ datetime.py
@@ -0,0 +1,27 @@
+# Python Datetime
+
+# Python Dates
+
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+# Date Output
+
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
+
+# Creating Date Objects
+
+x = datetime.datetime(2020, 5, 17)
+
+print(x)
+
+# The strftime() Method
+
+x = datetime.datetime(2018, 6, 1)
+
+print(x.strftime("%B"))
