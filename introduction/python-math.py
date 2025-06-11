b4b7936c3ce764caa0853b334de8be678053d8ee
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-06-11 15:41:17 +0330

    Python Math

---
+++ introduction/python-math.py
@@ -0,0 +1,29 @@
+# Python Math
+
+import math
+
+
+# Built-in Math Functions
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)
+print(y)
+
+x = abs(-7.25)
+print(x)
+
+x = pow(4, 3)
+print(x)
+
+
+# The Math Module
+x = math.sqrt(64)
+print(x)
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+print(x)
+print(y)
+
+x = math.pi
+print(x)
