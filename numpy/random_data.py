f838483a567e8fde00040be0b14948accfd988d4
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-07-09 06:35:25 +0330

    Random Data Distribution

---
+++ numpy/random_data.py
@@ -0,0 +1,11 @@
+# Random Data Distribution
+
+from numpy import random
+
+
+# Random Distribution
+x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(100))
+print(x)
+
+x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(3, 5))
+print(x)
