e356e59692919a99a8073e336be4e2e25db767fd
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-07-15 08:40:03 +0330

    NumPy ufuncs

---
+++ numpy/ufuncs.py
@@ -0,0 +1,20 @@
+# NumPy ufuncs
+
+import numpy as np
+
+
+# What is Vectorization
+x = [1, 2, 3, 4]
+y = [4, 5, 6, 7]
+z = []
+
+for i, j in zip(x, y):
+    z.append(i + j)
+print(z)
+
+# use ufuncs
+x = [1, 2, 3, 4]
+y = [4, 5, 6, 7]
+z = np.add(x, y)
+
+print(z)
