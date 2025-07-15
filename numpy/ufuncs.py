d59bf3a19e9d9ec25d64819bae2209b9cb88028b
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-07-15 08:49:48 +0330

    Create Your Own ufunc

--- numpy/ufuncs.py
+++ numpy/ufuncs.py
@@ -18,3 +18,28 @@ y = [4, 5, 6, 7]
 z = np.add(x, y)
 
 print(z)
+
+
+# Create Your Own ufunc
+def myadd(x, y):
+    return x + y
+
+
+myadd = np.frompyfunc(myadd, 2, 1)
+
+print(myadd([1, 2, 3, 4], [5, 6, 7, 8]))
+
+# Check if a Function is a ufunc
+print(type(np.add))
+
+print(type(np.concatenate))
+
+"""
+print(type(np.blahblah)) 
+"""
+
+# Use an if statement to check if the function is a ufunc or not
+if type(np.add) == np.ufunc:
+    print("add is ufunc")
+else:
+    print("add is not ufunc")
