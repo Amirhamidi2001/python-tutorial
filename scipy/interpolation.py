631c147bef47106b989fd8384a2cf6d6d95adc0e
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-07-30 16:44:24 +0330

    SciPy Interpolation

---
+++ scipy/interpolation.py
@@ -0,0 +1,26 @@
+# SciPy Interpolation
+
+from scipy.interpolate import interp1d, UnivariateSpline, Rbf
+import numpy as np
+
+
+# 1D Interpolation
+xs = np.arange(10)
+ys = 2 * xs + 1
+interp_func = interp1d(xs, ys)
+newarr = interp_func(np.arange(2.1, 3, 0.1))
+print(newarr)
+
+# Spline Interpolation
+xs = np.arange(10)
+ys = xs**2 + np.sin(xs) + 1
+interp_func = UnivariateSpline(xs, ys)
+newarr = interp_func(np.arange(2.1, 3, 0.1))
+print(newarr)
+
+# Interpolation with Radial Basis Function
+xs = np.arange(10)
+ys = xs**2 + np.sin(xs) + 1
+interp_func = Rbf(xs, ys)
+newarr = interp_func(np.arange(2.1, 3, 0.1))
+print(newarr)
