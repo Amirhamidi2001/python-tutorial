71d6b91fe6ac1db358ef4aef72c3f5d3777fda80
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-08-10 21:58:07 +0330

    Test variable comparison in template
    - Modified template.html to check if two variables refer to the same object using 'is'
    - Updated 'testing' view to pass two sample lists (x and y) instead of member data

--- django/my_tennis_club/members/views.py
+++ django/my_tennis_club/members/views.py
@@ -27,9 +27,9 @@ def main(request):
 
 
 def testing(request):
-    mymembers = Member.objects.all().values()
     template = loader.get_template("template.html")
     context = {
-        "mymembers": mymembers,
+        "x": ["Apple", "Banana", "Cherry"],
+        "y": ["Apple", "Banana", "Cherry"],
     }
     return HttpResponse(template.render(context, request))
