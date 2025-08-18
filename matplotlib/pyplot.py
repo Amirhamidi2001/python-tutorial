09ff3a882d47292a185644e1387c8e6712b670a0
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-08-18 20:40:28 +0330

    Matplotlib Pyplot

---
+++ matplotlib/pyplot.py
@@ -0,0 +1,11 @@
+# Matplotlib Pyplot
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Pyplot
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+plt.plot(xpoints, ypoints)
+plt.show()
