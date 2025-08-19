d80ad25d49dc4a3fecd34e62ffe9644245f89bfd
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-08-19 17:08:40 +0330

    Matplotlib Plotting

---
+++ matplotlib/plotting.py
@@ -0,0 +1,26 @@
+# Matplotlib Plotting
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Plotting x and y points
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+plt.plot(xpoints, ypoints)
+plt.show()
+
+# Plotting Without Line
+plt.plot(xpoints, ypoints, "o")
+plt.show()
+
+# Multiple Points
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(xpoints, ypoints)
+plt.show()
+
+# Default X-Points
+ypoints = np.array([3, 8, 1, 10, 5, 7])
+plt.plot(ypoints)
+plt.show()
