6e8935b04dfc665922810e069403819a8035aedd
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-08-28 05:29:14 +0330

    Data Distribution

---
+++ machinelearning/data_distribution.py
@@ -0,0 +1,19 @@
+# Data Distribution
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# How Can we Get Big Data Sets?
+x = np.random.uniform(0.0, 5.0, 250)
+print(x)
+
+# Histogram
+x = np.random.uniform(0.0, 5.0, 250)
+plt.hist(x, 5)
+plt.show()
+
+# Big Data Distributions
+x = np.random.uniform(0.0, 5.0, 100000)
+plt.hist(x, 100)
+plt.show()
