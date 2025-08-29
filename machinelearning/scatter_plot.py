b2a7db84085d33385a1ffcc1427d6fc37177b155
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-08-29 10:22:44 +0330

    Machine Learning - Scatter Plot

---
+++ machinelearning/scatter_plot.py
@@ -0,0 +1,17 @@
+# Machine Learning - Scatter Plot
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Scatter Plot
+x = [5, 7, 8, 7, 2, 17, 2, 9, 4, 11, 12, 9, 6]
+y = [99, 86, 87, 88, 111, 86, 103, 87, 94, 78, 77, 85, 86]
+plt.scatter(x, y)
+plt.show()
+
+# Random Data Distributions
+x = np.random.normal(5.0, 1.0, 1000)
+y = np.random.normal(10.0, 2.0, 1000)
+plt.scatter(x, y)
+plt.show()
