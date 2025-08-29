5cce3e605b41b1b15406de5ee13c7fd50d5d0cbf
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-08-29 10:06:17 +0330

    Machine Learning - Normal Data Distribution

--- machinelearning/data_distribution.py
+++ machinelearning/data_distribution.py
@@ -17,3 +17,8 @@ plt.show()
 x = np.random.uniform(0.0, 5.0, 100000)
 plt.hist(x, 100)
 plt.show()
+
+# Normal Data Distribution
+x = np.random.normal(5.0, 1.0, 100000)
+plt.hist(x, 100)
+plt.show()
