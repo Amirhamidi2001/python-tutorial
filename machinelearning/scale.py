93074e83ed71fee7ef96469438505393fdfeaf63
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-09-02 19:40:16 +0330

    Machine Learning - Scale

---
+++ machinelearning/scale.py
@@ -0,0 +1,25 @@
+# Machine Learning - Scale
+
+import pandas
+from sklearn import linear_model
+from sklearn.preprocessing import StandardScaler
+
+
+# Scale Features
+scale = StandardScaler()
+df = pandas.read_csv("data.csv")
+X = df[["Weight", "Volume"]]
+scaledX = scale.fit_transform(X)
+print(scaledX)
+
+# Predict CO2 Values
+scale = StandardScaler()
+df = pandas.read_csv("data.csv")
+X = df[["Weight", "Volume"]]
+y = df["CO2"]
+scaledX = scale.fit_transform(X)
+regr = linear_model.LinearRegression()
+regr.fit(scaledX, y)
+scaled = scale.transform([[2300, 1.3]])
+predictedCO2 = regr.predict([scaled[0]])
+print(predictedCO2)
