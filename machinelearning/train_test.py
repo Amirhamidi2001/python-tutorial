037c5d1fb234ce064d55fcf2d1f9cd3ae5416dca
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-09-03 21:09:03 +0330

    Machine Learning - Train/Test

---
+++ machinelearning/train_test.py
@@ -0,0 +1,54 @@
+# Machine Learning - Train/Test
+
+import numpy
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
+
+
+# Start With a Data Set
+numpy.random.seed(2)
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+plt.scatter(x, y)
+plt.show()
+
+# Fit the Data Set
+numpy.random.seed(2)
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+train_x = x[:80]
+train_y = y[:80]
+test_x = x[80:]
+test_y = y[80:]
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+myline = numpy.linspace(0, 6, 100)
+plt.scatter(train_x, train_y)
+plt.plot(myline, mymodel(myline))
+plt.show()
+
+# R2
+numpy.random.seed(2)
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+train_x = x[:80]
+train_y = y[:80]
+test_x = x[80:]
+test_y = y[80:]
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+r2 = r2_score(train_y, mymodel(train_x))
+print(r2)
+
+# Bring in the Testing Set
+numpy.random.seed(2)
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+train_x = x[:80]
+train_y = y[:80]
+test_x = x[80:]
+test_y = y[80:]
+mymodel = numpy.poly1d(numpy.polyfit(train_x, train_y, 4))
+r2 = r2_score(test_y, mymodel(test_x))
+print(r2)
+
+# Predict Values
+print(mymodel(5))
