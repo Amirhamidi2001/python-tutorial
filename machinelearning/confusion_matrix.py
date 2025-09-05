6cf90c2d239a4ba00de24a7117e2da4722e460cb
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-09-05 09:55:14 +0330

    Machine Learning - Confusion Matrix

---
+++ machinelearning/confusion_matrix.py
@@ -0,0 +1,19 @@
+# Machine Learning - Confusion Matrix
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import metrics
+
+
+# Creating a Confusion Matrix
+actual = np.random.binomial(1, 0.9, size=1000)
+predicted = np.random.binomial(1, 0.9, size=1000)
+
+confusion_matrix = metrics.confusion_matrix(actual, predicted)
+
+cm_display = metrics.ConfusionMatrixDisplay(
+    confusion_matrix=confusion_matrix, display_labels=[0, 1]
+)
+
+cm_display.plot()
+plt.show()
