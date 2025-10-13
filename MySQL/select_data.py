8e01d13947a9b9191cc6f677b217f75196ca4e82
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-10-13 21:10:41 +0330

    Python MySQL Delete From By

--- MySQL/select_data.py
+++ MySQL/select_data.py
@@ -74,3 +74,18 @@ myresult = mycursor.fetchall()
 
 for x in myresult:
     print(x)
+
+# Delete Record
+sql = "DELETE FROM customers WHERE address = 'Mountain 21'"
+mycursor.execute(sql)
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) deleted")
+
+# Prevent SQL Injection
+sql = "DELETE FROM customers WHERE address = %s"
+adr = ("Yellow Garden 2",)
+mycursor.execute(sql, adr)
+mydb.commit()
+
+print(mycursor.rowcount, "record(s) deleted")
