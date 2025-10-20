d71bb7f92756995c13f2abea80b7bf2686ce499e
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-10-20 09:31:01 +0330

    Python Statements

---
+++ introduction/statements.py
@@ -0,0 +1,14 @@
+# Python Statements
+
+# Statements
+print("Python is fun!")
+
+# Many Statements
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+# Semicolons (Optional, Rarely Used)
+print("Hello")
+print("How are you?")
+print("Bye bye!")
