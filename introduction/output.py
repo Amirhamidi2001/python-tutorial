71bed320c4587d9b1d707db1f07e3084e125f8e4
Amir Hamidi <Amirarsalanh2001@gmail.com>
2025-10-20 09:37:10 +0330

    Python Output / Print

---
+++ introduction/output.py
@@ -0,0 +1,9 @@
+# Python Output / Print
+
+# Print Without a New Line
+
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+# Mix Text and Numbers
+print("I am", 35, "years old.")
